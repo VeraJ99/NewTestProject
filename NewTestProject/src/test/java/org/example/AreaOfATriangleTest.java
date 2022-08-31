3dfb51eea5c2f13f6dd8a9641209a5b39b0931c6
VeraJ99 <90844547+VeraJ99@users.noreply.github.com>
2022-08-31 14:29:45 +0300

    Update AreaOfATriangleTest.java

--- NewTestProject/src/test/java/org/example/AreaOfATriangleTest.java
+++ NewTestProject/src/test/java/org/example/AreaOfATriangleTest.java
@@ -14,7 +14,7 @@ public class AreaOfATriangleTest {
         AreaOfATriangle areaOfATriangle = new AreaOfATriangle();
 
 
-        Assertions.assertEquals(6.49519052838329, areaOfATriangle.countTriangleArea(3, 5, 7));
+        Assertions.assertEquals(6.5, areaOfATriangle.countTriangleArea(3, 5, 7));
         Assertions.assertThrows(MySideOfTriangleException.class, () -> areaOfATriangle.countTriangleArea(-3, 5, 7));
         Assertions.assertThrows(MyFigureNotTriangleException.class, () -> areaOfATriangle.countTriangleArea(1, 5, 7));
     }
